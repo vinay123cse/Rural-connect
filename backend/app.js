38df2d20fc19ae88b516b1740aae3fb5e955c943
Vinay Kumar <vy2630066@gmail.com>
2026-03-23 05:21:51 +0530

    app.js fixed

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
