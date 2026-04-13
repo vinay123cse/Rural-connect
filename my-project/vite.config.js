28e23ce9cd1749f46d23f064054acd4bc0205162
Vinay Kumar <vy2630066@gmail.com>
2026-04-14 02:13:30 +0530

    Fixed 404 on refresh with vite base path

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Yeh line ensure karegi ki build ke baad bhi assets sahi load ho
 })
