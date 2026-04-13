1003b28f8e312a0eee8e0731755a1e08885f7e8d
Vinay Kumar <vy2630066@gmail.com>
2026-04-14 02:22:16 +0530

    Fix: Socket connection 404 on production

--- my-project/src/socket/socket.js
+++ my-project/src/socket/socket.js
@@ -1,14 +1,24 @@
+// import { io } from "socket.io-client";
+
+// // URL ko variable mein rakhna accha rehta hai
+// const SOCKET_URL = "https://rural-connect-6jba.onrender.com";
+// // const SOCKET_URL = "http://localhost:3000";
+
+// const socket = io(SOCKET_URL, {
+//     autoConnect: true,        // Apne aap connect ho jaye
+//     reconnection: true,       // Agar server down ho toh try karta rahe
+//     reconnectionAttempts: 5,  // Kitni baar try kare
+//     transports: ["websocket"] // Tezi ke liye direct websocket use kare
+// });
+
+// export default socket;
+
 import { io } from "socket.io-client";
 
-// URL ko variable mein rakhna accha rehta hai
-const SOCKET_URL = "https://rural-connect-6jba.onrender.com";
-// const SOCKET_URL = "http://localhost:3000";
-
-const socket = io(SOCKET_URL, {
-    autoConnect: true,        // Apne aap connect ho jaye
-    reconnection: true,       // Agar server down ho toh try karta rahe
-    reconnectionAttempts: 5,  // Kitni baar try kare
-    transports: ["websocket"] // Tezi ke liye direct websocket use kare
+// Khali chhodne se ye wahi URL uthayega jahan se site chal rahi hai
+const socket = io("/", {
+    transports: ["polling", "websocket"],
+    withCredentials: true
 });
 
 export default socket;
